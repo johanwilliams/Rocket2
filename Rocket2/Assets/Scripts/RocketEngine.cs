35e03e7bc0bd5ed8a91860d0b9bf7c7acf695239
Johan Williams <williams.johan@gmail.com>
2018-03-29 12:44:11 +0200

    Added a rocket flame (particle system)

--- Rocket2/Assets/Scripts/RocketEngine.cs
+++ Rocket2/Assets/Scripts/RocketEngine.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -12,49 +12,66 @@ public class RocketEngine : MonoBehaviour {
     private float thrusterForce = 1000f;
     private float thruster = 0f;
 
+    [SerializeField]
+    private Transform rocketFlame;
+    private bool rocketFlameEnabled = false;
+
     private Rigidbody2D rb;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         rb = GetComponent<Rigidbody2D>();
-		
-	}
+        SetRocketFlame(false);
+    }
 
     // Sets the rocket engine rotation by taking a roation input (between -1 and 1) and adds the rotation speed.
-    public void ApplyRotation(float _rotation)
-    {
+    public void ApplyRotation(float _rotation) {
         rotation = _rotation * rotationSpeed;
     }
 
     // Sets the rocket engine thruster by taking a thruster input (between -1 and 1) and adds the thruster force.
-    public void ApplyThruster(float _thruster)
-    {
+    public void ApplyThruster(float _thruster) {
         thruster = _thruster * thrusterForce;
     }
 
+    // Run every graphics update
+    private void Update() {
+        UpdateRocketFlame();
+    }
+
     // Run every physics iteration
-    void FixedUpdate()
-    {
+    private void FixedUpdate() {
         PerformRotation();
         PerformMovement();
     }
 
     // Rotats the rocket
-    private void PerformRotation()
-    {
+    private void PerformRotation() {
         if (Mathf.Abs(rotation) > 0f) {
             rb.MoveRotation(rb.rotation - rotation * Time.fixedDeltaTime);
         }
     }
 
     // Moves the rocket (applies thruster force)
-    private void PerformMovement()
-    {
+    private void PerformMovement() {
         // Only apply thruster if positive (i.e. no reverse thruster)
         if (thruster > 0f) {
-            rb.AddForce(rb.transform.up * thruster * Time.fixedDeltaTime);            
+            rb.AddForce(rb.transform.up * thruster * Time.fixedDeltaTime);
+        }
+    }
 
+    private void UpdateRocketFlame() {
+        bool thrustersOn = (thruster > 0f);
+        if (thrustersOn != rocketFlameEnabled) {
+            rocketFlameEnabled = thrustersOn;
+            SetRocketFlame(rocketFlameEnabled);
+        }
+    }
 
+    private void SetRocketFlame(bool _enabled) {
+        foreach (Transform child in rocketFlame) {
+            ParticleSystem.EmissionModule em = child.GetComponent<ParticleSystem>().emission;
+            em.enabled = _enabled;
         }
     }
 }
