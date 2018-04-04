29e0e559b583e30b3ad178eaec0344e2bba21d09
johanwilliams <williams.johan@gmail.com>
2018-04-04 23:19:48 +0200

    More work on the login menu and the lobby

---
+++ Rocket2/Assets/UserAccountLobby.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserAccountLobby : MonoBehaviour {
+
+    public Text usernameText;
+
+    private void Start() {
+        if (UserAccountManager.isLoggedIn)
+            usernameText.text = UserAccountManager.playerUsername;
+    }
+
+    public void LogOut() {
+        if (UserAccountManager.isLoggedIn)
+            UserAccountManager.instance.LogOut();
+    }
+}
