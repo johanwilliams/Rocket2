65d7fa85d15e715b1f0b0d2b39fdc8da80801e85
johanwilliams <williams.johan@gmail.com>
2018-04-18 23:05:11 +0200

    Weapon refactoring

--- Rocket2/Assets/Scripts/Rocket/RocketController.cs
+++ Rocket2/Assets/Scripts/Rocket/RocketController.cs
@@ -2,14 +2,22 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 [RequireComponent(typeof(RocketEngine))]
+[RequireComponent(typeof(WeaponManager))]
+[RequireComponent(typeof(Player))]
 public class RocketController : NetworkBehaviour {
 
     // Component caching
     private RocketEngine rocketEngine;
+    private WeaponManager weaponManager;
+    private Player player;
+
+    public bool lockCursor;
 
     // Use this for initialization
     void Start () {
         rocketEngine = GetComponent<RocketEngine>();
+        weaponManager = GetComponent<WeaponManager>();
+        player = GetComponent<Player>();
     }
 	
 	// Update is called once per frame
@@ -18,7 +26,7 @@ public class RocketController : NetworkBehaviour {
             return;
 
         if (PauseMenu.IsOn) {
-            if (Cursor.lockState != CursorLockMode.None)
+            if (Cursor.lockState != CursorLockMode.None && lockCursor)
                 Cursor.lockState = CursorLockMode.None;
             rocketEngine.ApplyRotation(0f);
             rocketEngine.ApplyThruster(0f);
@@ -26,7 +34,7 @@ public class RocketController : NetworkBehaviour {
             return;
         }
 
-        if (Cursor.lockState != CursorLockMode.Locked)
+        if (Cursor.lockState != CursorLockMode.Locked && lockCursor)
             Cursor.lockState = CursorLockMode.Locked;
 
         // Get the horizontal and vertical input
@@ -38,5 +46,19 @@ public class RocketController : NetworkBehaviour {
 
         // Send the vertical/thruster input to the rocket engine
         rocketEngine.ApplyThruster(_inputVer);
+
+        //TODO: For debug only
+        if (Input.GetKeyDown(KeyCode.K))
+            weaponManager.CmdDamageGameObject(player.gameObject, player.name, 50);
+
+        if (Input.GetButtonDown("Fire1"))
+            weaponManager.Fire(Weapon.Slot.Primary);
+        else if (Input.GetButtonUp("Fire1"))
+            weaponManager.Ceasefire(Weapon.Slot.Primary);
+
+        if (Input.GetButtonDown("Fire2"))
+            weaponManager.Fire(Weapon.Slot.Seconday);
+        else if (Input.GetButtonUp("Fire2"))
+            weaponManager.Ceasefire(Weapon.Slot.Seconday);
     }
 }
--- Rocket2/Assets/Scripts/Rocket/RocketShoot.cs
+++ Rocket2/Assets/Scripts/Rocket/RocketShoot.cs
@@ -25,15 +25,13 @@ public class RocketShoot : NetworkBehaviour {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    private void OnDisable() {
-        // Stop autofire
-        CancelInvoke("Shoot");
-    }
-
     private void Update() {
         if (PauseMenu.IsOn || !isLocalPlayer)
             return;
 
+        return;
+
+        //TODO: For debug only
         if (Input.GetKeyDown(KeyCode.K))
             weaponManager.CmdDamageGameObject(player.gameObject, player.name, 50);
 
@@ -48,103 +46,4 @@ public class RocketShoot : NetworkBehaviour {
             weaponManager.Ceasefire(Weapon.Slot.Seconday);
     }
 
-    // Check that we are not cheating the rapid fire but tapping fire
-    private bool isShootingAllowed() {
-        // Check the time since last shot
-        if ((Time.time - lastShotTime) < 1f / primaryWeapon.fireRate)
-            return false;        
-
-        return true;
-    }
-
-    // Called on the server when a player shoots
-    [Command]
-    void CmdOnShoot(Vector3 _hitPos) {
-        RpcDoShootEffect(_hitPos);
-    }
-
-    // Called on all clients when we need to do a shoot effect
-    [ClientRpc]
-    void RpcDoShootEffect(Vector3 _hitPos) {
-        weaponManager.GetCurrentWeaponGraphics().mussleFlash.Play();
-        RenderTrail(_hitPos);
-        AudioManager.instance.PlayClipAtPoint("LaserShot", transform.position);
-    }
-
-    // Renders a weapon trail (if we have one configured)
-    private void RenderTrail(Vector3 _hitPos) {
-        GameObject trailEffectPrefab = weaponManager.GetCurrentWeaponGraphics().trailEffectPrefab;
-        if (trailEffectPrefab != null) {
-            Transform _weaponSlot = weaponManager.GetWeaponSlot();
-            GameObject trailClone = Instantiate(trailEffectPrefab, _weaponSlot.position, _weaponSlot.rotation);            
-            LineRenderer lr = trailClone.GetComponent<LineRenderer>();
-            if (lr != null) {
-                lr.SetPosition(0, _weaponSlot.transform.position);
-                lr.SetPosition(1, _hitPos);
-            }
-            Destroy(trailClone, 0.04f);
-        }
-    }
-
-    // Called on the server when we hit something. Takes the hitpoint and the normal of the hit surface as parameters
-    [Command]
-    void CmdOnHit(Vector3 _hitPos, Vector3 _normal) {
-        RpcDoHitEffect(_hitPos, _normal);
-    }
-
-    // Called on all clienct to show hit effect
-    [ClientRpc]
-    void RpcDoHitEffect(Vector3 _hitPos, Vector3 _normal) {
-
-        //TODO: Object pooling could come in useful here if we do a lot of instantiating
-        GameObject _hitEffect = Instantiate(weaponManager.GetCurrentWeaponGraphics().hitEffectPrefab, _hitPos, Quaternion.LookRotation(_normal));
-        Destroy(_hitEffect, 2f);
-    }
-
-    [Client]
-    void Shoot() {
-        // Only do the raycast if we are the local player
-        if (!isLocalPlayer)
-            return;
-
-        // Check that we have enough energy        
-        if (primaryWeapon.energyCost > player.energy.GetEnergy())
-            return;
-
-        // Add the recoil (todo: Maybe refactor to a separate AddForce method as we want to add forces in other situations)
-        rb.AddForce(-rb.transform.up * primaryWeapon.recoil * Time.deltaTime, ForceMode2D.Impulse);
-
-        // Raycast to detect if we hit something (should perhaps be refactored for the specific weapon?)
-        Transform _weaponSlot = weaponManager.GetWeaponSlot();
-        RaycastHit2D _hit = Physics2D.Raycast(_weaponSlot.transform.position, _weaponSlot.transform.up, primaryWeapon.range, mask);
-        Debug.DrawLine(_weaponSlot.transform.position, _weaponSlot.transform.position + _weaponSlot.transform.up * primaryWeapon.range, Color.cyan);
-
-        Vector3 _hitPos = _weaponSlot.transform.position + _weaponSlot.transform.up * primaryWeapon.range;
-        if (_hit.collider != null) {
-            if (_hit.collider.tag == PLAYER_TAG) {
-                CmdPlayerShot(_hit.collider.name, primaryWeapon.damage, transform.name);
-            }
-            _hitPos = _hit.point;
-
-            // Call the OnHit method on the server
-            CmdOnHit(_hitPos, _hit.normal);
-        }
-
-        // Call the OnShoot method on the server
-        CmdOnShoot(_hitPos);
-
-        lastShotTime = Time.time;
-
-        player.energy.ConsumeEnergy(primaryWeapon.energyCost);
-    }
-
-    // Command (server side method) which takes care of a player shooting another player
-    [Command]
-    void CmdPlayerShot (string _playerID, int _damage, string _sourcePlayerID) {        
-
-        // Get the player being shot from the game manager so we can damage him
-        Player _player = GameManager.GetPlayer(_playerID);
-        //_player.RpcTakeDamage(_damage, _sourcePlayerID);
-        _player.health.RpcTakeDamage(_damage, _sourcePlayerID);
-    }
 }
