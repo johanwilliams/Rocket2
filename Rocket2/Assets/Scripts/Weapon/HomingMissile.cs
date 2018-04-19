24bcc063d281e4cfe54cd4b99e0581255f78d126
johan <williams.johan@gmail.com>
2018-04-19 10:03:11 +0200

    v1.0 of a homing missile

--- Rocket2/Assets/Scripts/Weapon/HomingMissile.cs
+++ Rocket2/Assets/Scripts/Weapon/HomingMissile.cs
@@ -5,26 +5,48 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class HomingMissile : MonoBehaviour {
 
+    private enum State { Launched, Searching, Locked };
+
     private Rigidbody2D rb;
 
     [SerializeField] private float speed = 5f;
     [SerializeField] private float rotateSpeed = 200f;
+    [SerializeField] private float searchStartTime = 1f;
+    [SerializeField] private float searchRate = 2f;
+
+    private State state;
 
     public Transform target;
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
+        state = State.Launched;
+        StartCoroutine(SearchForTarget());
 	}
+
+    IEnumerator SearchForTarget() {
+        Debug.Log("Launched");
+        yield return new WaitForSeconds(searchStartTime);
+        state = State.Searching;
+        while(target != null) {
+            Debug.Log("Searching");
+            yield return new WaitForSeconds(1f / searchRate);    
+        }
+        yield return true;
+    }
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-        Vector2 direction = (Vector2) target.position - rb.position;
-        direction.Normalize();
+	void FixedUpdate () {                
+        //Rotate
+        if (target != null) {
+            Vector2 dirToTarget = (Vector2)target.position - rb.position;
+            dirToTarget.Normalize();
+            float rotateAmount = Vector3.Cross(dirToTarget, transform.up).z;
+            rb.angularVelocity = -rotateAmount * rotateSpeed;
+        }
 
-        float rotateAmount = Vector3.Cross(direction, transform.up).z;
-
-        rb.angularVelocity = -rotateAmount * rotateSpeed;
+        //Move
         rb.velocity = transform.up * speed;
 	}
 
