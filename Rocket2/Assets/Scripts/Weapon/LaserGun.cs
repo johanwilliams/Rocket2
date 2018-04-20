99e6c14fb4668c31c0862025d165e7e8be1cc5ea
johan <williams.johan@gmail.com>
2018-04-20 15:06:51 +0200

    Weapon and projectile refactoring

--- Rocket2/Assets/Scripts/Weapon/LaserGun.cs
+++ Rocket2/Assets/Scripts/Weapon/LaserGun.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class LaserGun : LaserWeapon {
 
-    // Simple lasergun that don't need any modificxations from the base class
-   
+    // Simple lasergun that don't need any modifications from the base class
+    protected override void Start() {
+        base.Start();
+        displayName = "a lasergun";
+    }
+
 }
