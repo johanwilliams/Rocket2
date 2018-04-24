122ca959748b6ce5af344c83f53d8a70c834f71d
johanwilliams <williams.johan@gmail.com>
2018-04-24 21:52:10 +0200

    Weapon refactoring

--- Rocket2/Assets/Scripts/Weapon/Gun.cs
+++ Rocket2/Assets/Scripts/Weapon/Gun.cs
@@ -3,15 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class Gun : Weapon {
+public class Gun : ProjectileWeapon {
 
-    public GameObject bulletPrefab;    
-
-    public override void Shoot(Player shooter) {
-        base.Shoot(shooter);
-
-        var bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        NetworkServer.Spawn(bullet);
-
+    protected override void Start() {
+        base.Start();
+        displayName = "a gun";
     }
 }
--- Rocket2/Assets/Scripts/Weapon/MissileLauncher.cs
+++ Rocket2/Assets/Scripts/Weapon/MissileLauncher.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class MissileLauncher : ProjectileWeapon {
 
-    // Simple lasergun that don't need any modifications from the base class
     protected override void Start() {
         base.Start();
         displayName = "a missile launcher";
