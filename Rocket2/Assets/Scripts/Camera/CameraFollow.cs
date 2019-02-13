bad9b41054ae50202e64664c5c61dd113ffd0732
johanwilliams <williams.johan@gmail.com>
2019-02-13 22:26:43 +0100

    Resolved a bug for the camera being jittery - now updates its movement in FixedUpdate

--- Rocket2/Assets/Scripts/Camera/CameraFollow.cs
+++ Rocket2/Assets/Scripts/Camera/CameraFollow.cs
@@ -8,7 +8,7 @@ public class CameraFollow : MonoBehaviour {
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
 
-    private void LateUpdate() {
+    private void FixedUpdate() {
         if (target != null) {
             Vector3 desiredPosition = target.position + offset;
             Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
--- Rocket2/Assets/Scripts/Player/Player.cs
+++ Rocket2/Assets/Scripts/Player/Player.cs
@@ -132,8 +132,9 @@ public class Player : NetworkBehaviour {
 
     private void UpdateScore(string _sourcePlayerID) {
         // Update kill/death stats
-        Debug.Log("Player.cs: " + _sourcePlayerID);
+        Debug.Log("Updateing score: " + _sourcePlayerID);
         Player sourcePlayer = GameManager.GetPlayer(_sourcePlayerID);
+        //TODO: If we kill outself (sourceid = null) we do not update the server of the score
         if (sourcePlayer != null) {
             GameManager.instance.onPlayerKilledCallback.Invoke(username, sourcePlayer.username);
 
